0d20f7db187af6195add24a743b231df5e5da1c7
Rishabh Verma <rishabhverma.rv@gmail.com>
2020-09-02 14:13:17 -0700

    First commit.

---
+++ main.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(rwt)
+
+data <- read_csv("data/uncalibrated.csv")
+
+gyroscope <- data %>% 
+  filter(type == 4) %>% 
+  select(time, one, two, three) %>% 
+  arrange(desc(time))
+
+
+
+WINDOW_LENGTH = 128
+chunks = floor(gyroscope %>% count %>% as.integer / WINDOW_LENGTH)
+samples = WINDOW_LENGTH*chunks
+
+gyroscope <- gyroscope %>% slice(1:samples)
+
+smooth_data <- gyroscope %>% 
+  select(one) %>% 
+  unlist()
+browser()
+h <- daubcqf(4)  # must be even
+
+one_smooth <- denoise.dwt(smooth_data, h$h.0) %>% as_tibble()
+
+one_smooth
+
+# one_smooth <- bind_cols(gyroscope, one_smooth)
+
